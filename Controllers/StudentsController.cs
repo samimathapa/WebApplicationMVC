f9cd886d2be918aff845ff7934906c40c3e0c3c9
samimathapa <41064427+samimathapa@users.noreply.github.com>
2022-11-12 08:50:10 -0500

    Add files via upload

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Models;
+
+namespace MyWebApp.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Details", std);
+            }
+            else
+            {
+                return View(std);
+            }
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace MyWebApp.Models
+{
+    public class Student
+    {
+        [Required]
+        public int StdID { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Enter the Student Name")]
+        [StringLength(30, ErrorMessage = "Please enter less than 30 characters")]
+        
+public string Name { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Enter the Student Address")]
+        [StringLength(30, ErrorMessage = "Please enter less than 30 characters")]
+        public string Address { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Enter the Student Faculty")]
+        public string Faculty { get; set; }
+        [Required(ErrorMessage = "Enter the Student Phone Number")]
+        public long PhoneNo { get; set; }
+    }
+}
